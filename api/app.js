ec3494c18fdf747fc25543c6e0039e8d82f49b19
George Walker <george.walker.wme@gmail.com>
2023-01-10 11:50:44 +0000

    Cors

--- api/app.js
+++ api/app.js
@@ -22,15 +22,14 @@ dotenv.config({ path: './config/config.env' })
 connectDB()
 
 const app = express();
-const whitelist = ['https://my-site-diary.netlify.app', 'http://localhost:3000/']
 
-// const corsOptions ={
-//    origin:'https://my-site-diary.netlify.app', 
-//    credentials: true,
-//    optionSuccessStatus: 200,
-// }
+const corsOptions ={
+   origin:'https://my-site-diary.netlify.app', 
+   credentials: true,
+   optionSuccessStatus: 200,
+}
 
-app.use(cors()) 
+app.use(cors(corsOptions))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({
