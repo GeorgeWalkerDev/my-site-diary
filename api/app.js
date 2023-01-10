20a01e859a12d1989f01e899de6b1a8094797fda
George Walker <george.walker.wme@gmail.com>
2023-01-10 11:47:07 +0000

    Edit Diary updated

--- api/app.js
+++ api/app.js
@@ -24,13 +24,13 @@ connectDB()
 const app = express();
 const whitelist = ['https://my-site-diary.netlify.app', 'http://localhost:3000/']
 
-const corsOptions ={
-   origin:'https://my-site-diary.netlify.app', 
-   credentials: true,
-   optionSuccessStatus: 200,
-}
+// const corsOptions ={
+//    origin:'https://my-site-diary.netlify.app', 
+//    credentials: true,
+//    optionSuccessStatus: 200,
+// }
 
-app.use(cors(corsOptions)) 
+app.use(cors()) 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({
---
+++ client/src/components/EditDiaryForm.js
@@ -0,0 +1,110 @@
+import Container from '@mui/material/Container'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+import FormGroup from '@mui/material/FormGroup'
+import FormControl from '@mui/material/FormControl'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button' 
+import { Link as RouterLink, useParams, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { useUpdateDiaryMutation } from '../features/diaries/diariesApiSlice'
+
+const EditDiary = ({ diary }) => {
+
+    const [updateNote, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useUpdateNoteMutation()
+
+    const navigate = useNavigate();
+
+    const [project, setProject] = useState('')
+    const [weather, setWeather] = useState('')
+    const [resource, setResource] = useState('')
+    const [delays, setDelays] = useState('')
+    const [variations, setVariations] = useState('')
+    const [healthsafety, setHealthSafety] = useState('')
+    const [deliveries, setDeliveries] = useState('')
+    const [notes, setNotes] = useState('')
+    const [date, setDate] = useState('')
+
+    useEffect(() => {
+
+        if (isSuccess ) {
+            setProject('')
+            setWeather('')
+            setResource('')
+            setDelays('')
+            setVariations('')
+            setHealthSafety('')
+            setDeliveries('')
+            setNotes('')
+            setDate('')
+            navigate('/dashboard/diaries')
+        }
+
+    }, [isSuccess, navigate])
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        if (!isLoading){
+            const editedDiary = {id, date, project, weather, resource, delays, variations, healthsafety, deliveries, notes}
+            await updateNote({editedDiary})
+        }
+    }
+
+  return (
+    <Container maxWidth="xs">
+        <Box sx={{py:4}}>
+            <Paper>
+                <Container sx={{py: 2}}>
+                    <Typography variant="h4" gutterBottom>Edit Diary</Typography>
+                    <form onSubmit={(e) => onSubmit(e)}>
+                        <FormGroup>
+                            <FormControl sx={{mt:2}}>
+                                <TextField variant="outlined" label="Project Name" multiline value={project} onChange={(e) => setProject(e.target.value)}/>
+                            </FormControl>
+                            <FormControl sx={{mt:2}}>
+                                <TextField variant="outlined" label="Weather" multiline value={weather} onChange={(e) => setWeather(e.target.value)}/>
+                            </FormControl>
+                            <FormControl sx={{mt:2}}>
+                                <TextField variant="outlined" label="Resource" multiline value={resource} onChange={(e) => setResource(e.target.value)}/>
+                            </FormControl>
+                            <FormControl sx={{mt:2}}>
+                                <TextField variant="outlined" label="Delays" multiline value={delays} onChange={(e) => setDelays(e.target.value)}/>
+                            </FormControl>
+                            <FormControl sx={{mt:2}}>
+                                <TextField variant="outlined" label="Variations" multiline value={variations} onChange={(e) => setVariations(e.target.value)}/>
+                            </FormControl>
+                            <FormControl sx={{mt:2}}>
+                                <TextField variant="outlined" label="Health & Safety" multiline value={healthsafety} onChange={(e) => setHealthSafety(e.target.value)}/>
+                            </FormControl>
+                            <FormControl sx={{mt:2}}>
+                                <TextField variant="outlined" label="Deliveries" multiline value={deliveries} onChange={(e) => setDeliveries(e.target.value)}/>
+                            </FormControl>
+                            <FormControl sx={{mt:2}}>
+                                <TextField variant="outlined" label="General Notes" multiline value={notes} onChange={(e) => setNotes(e.target.value)}/>
+                            </FormControl>
+                        </FormGroup>
+                        <Button sx={{mt: 2}} variant="contained" color="success" type="submit">Save</Button>
+                        <Button
+                            sx={{mt: 2, ml: 1}}
+                            variant="contained"
+                            color="error"
+                            component={RouterLink}
+                            to="/dashboard/diaries"
+                        >
+                            Cancel
+                        </Button>
+                    </form>
+                </Container>
+            </Paper>
+        </Box>
+    </Container>
+  )
+}
+
+export default EditDiary
